11dbbd5537620dbdf1e6ace9b4c09fdf2d86407d
visurachan <visurachandula@gmail.com>
2026-01-21 19:43:11 +0000

    Initial commit - backned project future

---
+++ src/main/java/com/future/future_backend/FutureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.future.future_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureBackendApplication.class, args);
+	}
+
+}
